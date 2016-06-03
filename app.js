d5d575a199fdd99767f485f5653bf9b0fd89e8e5
Andy Choi <andy.dh.choi@gmail.com>
2016-06-03 16:36:23 -0400

    move db functionality to main program entry point and add exit handlers

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// initialize database
-var db = require("./db");
-db.initialize();
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
