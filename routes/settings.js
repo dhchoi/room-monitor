ea992a0a47a9b90ef186b00b144def2c561567c1
Andy Choi <andy.dh.choi@gmail.com>
2016-06-05 04:18:13 -0400

    combine interval related routes to one

--- routes/settings.js
+++ routes/settings.js
@@ -3,26 +3,26 @@ var serialport = require("../serial");
 var express = require("express");
 var router = express.Router();
 
-router.get("/get-interval", function (req, res, next) {
-  debug("Responding interval with value: " + global.arduinoInterval);
+router.route("/interval")
+  .get(function (req, res, next) {
+    debug("Responding interval with value: " + global.arduinoInterval);
 
-  res.json({interval: global.arduinoInterval});
-});
+    res.json({interval: global.arduinoInterval});
+  })
+  .post(function (req, res, next) {
+    var interval = req.body.interval;
+    debug("User request for set-interval with value: " + interval);
 
-router.post("/set-interval", function (req, res, next) {
-  var interval = req.body.interval;
-  debug("User request for set-interval with value: " + interval);
+    serialport.write("set-interval," + interval, function (error) {
+      if (error) {
+        return next(new Error("Failed to set new interval on device."));
+      }
 
-  serialport.write("set-interval," + interval, function (error) {
-    if (error) {
-      return next(new Error("Failed to set new interval on device."));
-    }
-
-    setTimeout(function () { // to ensure that value gets updated
-      res.json({interval: global.arduinoInterval});
-    }, 300);
+      setTimeout(function () { // to ensure that value gets updated
+        res.json({interval: global.arduinoInterval});
+      }, 300);
+    });
   });
-});
 
 // error handling middleware
 router.use(function (err, req, res, next) {
