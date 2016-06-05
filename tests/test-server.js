e3fff9945d09c05223bf9b461ddb638880f472f4
Andy Choi <andy.dh.choi@gmail.com>
2016-06-05 04:23:51 -0400

    create server tests

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,26 +1,85 @@
-var expect = require("chai").expect;
+var Debug = require("debug");
+// Debug.enable("app:*");
+var debug = Debug("app:tests:server");
 
-var args = null;
-// describe.only(), describe.skip()
-describe("Test", function() {
-  beforeEach(function(){
-    args = 1;
-  });
+var chai = require("chai");
+chai.use(require("chai-http"));
+var server = chai.request(require("../app")); // app should be exported as express() within file
+var expect = chai.expect;
 
-  it("should do something", function () {
-    expect(args).to.deep.equal(1);
-  });
-  it("should do another thing", function () {
-    expect(1+3).to.equal(4);
+// cherry pick by using `describe.only()` or `describe.skip()`
+
+/*
+ * Route: /
+ */
+describe("Requesting GET /", function () {
+  it("should return status 200", function (done) {
+    server.get("/")
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        done(err);
+      });
   });
 });
 
-describe("Test2", function() {
-  it("should do something", function () {
-    var args = 1;
-    expect(args).to.deep.equal(1);
+
+/*
+ * Route: /settings
+ */
+var routeSettingsInterval = "/settings/interval";
+var setIntervalParams;
+
+describe("Requesting GET " + routeSettingsInterval, function () {
+  it("should return status 200", function (done) {
+    server.get(routeSettingsInterval)
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        done(err);
+      });
   });
-  it("should do another thing", function () {
-    expect(1+3).to.equal(5);
+  it("should be json", function (done) {
+    server.get(routeSettingsInterval)
+      .end(function (err, res) {
+        expect(res).to.be.json;
+        done(err);
+      });
+  });
+  it("should contain correct fields", function (done) {
+    server.get(routeSettingsInterval)
+      .end(function (err, res) {
+        expect(res.body).to.have.property("interval");
+        done(err);
+      });
+  });
+});
+
+describe("Requesting POST " + routeSettingsInterval, function () {
+  beforeEach(function () {
+    setIntervalParams = {interval: 60};
+  });
+
+  it("should return status 200", function (done) {
+    server.post(routeSettingsInterval)
+      .send(setIntervalParams)
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        done(err);
+      });
+  });
+  it("should be json", function (done) {
+    server.post(routeSettingsInterval)
+      .send(setIntervalParams)
+      .end(function (err, res) {
+        expect(res).to.be.json;
+        done(err);
+      });
+  });
+  it("should contain correct fields", function (done) {
+    server.post(routeSettingsInterval)
+      .send(setIntervalParams)
+      .end(function (err, res) {
+        expect(res.body).to.have.property("interval");
+        done(err);
+      });
   });
 });
